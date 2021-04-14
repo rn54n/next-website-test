cac3658cac49b26e4c741c694c4b4b6b26fe63be
Aaren San <info@aarensan.com>
2021-04-15 01:46:10 +0200

    test: add getStaticPaths

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,3 +1,21 @@
+// fetch all data
+// so next know how many pages and routes to create
+export const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  const paths = data.map((ninja) => {
+    return {
+      params: { id: ninja.id.toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
 const Details = () => {
   return (
     <div>
