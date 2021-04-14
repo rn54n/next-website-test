32e4d219aee6ba15d16fc631d30c36599c3d2012
Aaren San <info@aarensan.com>
2021-04-14 10:32:56 +0200

    test: add layout component

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,15 @@
 import Head from "next/head";
 import Link from "next/link";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <p>test</p>
       <p>test2</p>
       <Link href="/ninjas">
         <a>See Ninja Listing</a>
       </Link>
-      <Footer />
     </div>
   );
 }
