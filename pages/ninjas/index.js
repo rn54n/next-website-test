1b47b14ba70b81f4a4910e8bd58f517c668d32b6
Aaren San <info@aarensan.com>
2021-04-14 12:34:47 +0200

    test: add fetch and connect api, display data

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,9 +1,28 @@
 import styles from "../../styles/Ninjas.module.css";
 
-const Ninjas = () => {
+// function runs in build time
+// this function does not run in the browser
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  return {
+    props: {
+      ninjas: data,
+    },
+  };
+};
+
+const Ninjas = ({ ninjas }) => {
   return (
     <div>
       <h1>All Ninjas</h1>
+      {ninjas.map((ninja) => (
+        <div key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
+        </div>
+      ))}
     </div>
   );
 };
